5a187c81aa574d2a502e102488909871946243a5
俞澄 <1099312164@qq.com>
2026-03-29 00:09:28 +0800

    feat: add albums config

---
+++ src/config/albums.js
@@ -0,0 +1,10 @@
+export const albums = [
+  {
+    slug: "travel",
+    title: "旅行",
+    description: "2024年的旅行记录",
+    layout: "masonry",
+    cover: "/photos/travel/cover.jpg",
+    photosFolder: "/photos/travel"
+  }
+];
