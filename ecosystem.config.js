9328059f5f30eb0189dac8e05656a73392320601
俞澄 <1099312164@qq.com>
2026-06-08 00:11:31 +0800

    pm2进程管理

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [{
+    name: "photodance",
+    script: "node_modules/.bin/next",
+    args: "start -p 3000",
+    cwd: "./",
+    instances: 1,
+    exec_mode: "fork",
+    env: {
+      NODE_ENV: "production",
+    },
+    log_date_format: "YYYY-MM-DD HH:mm:ss",
+    error_file: "./logs/error.log",
+    out_file: "./logs/out.log",
+    merge_logs: true,
+    max_memory_restart: "512M",
+  }],
+};
